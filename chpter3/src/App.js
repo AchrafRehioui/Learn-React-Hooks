5d0da6a9d826c42defc181b19aa50f432bf8abf2
Achraf <achraf.rehioui.1@gmail.com>
2021-03-22 19:51:58 +0100

    putting all component in App statically

--- chpter3/src/App.js
+++ chpter3/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PostList from './post/PostList';
+import CreatePost from './post/CreatePost';
+import UserBar from './user/UserBar';
 
+const user = 'Achraf'
 const posts = [
-  { title: 'React Hooks', content: 'React Hooks!', author: 'Achraf' },
+  { title: 'React Hooks', content: 'React!', author: 'Achraf' },
   { title: 'Using React Fragments', content: 'React Hooks!', author: 'Achraf' }
 ]
 
-export default function App() {
-  return <PostList posts={posts} />
 
+export default function App() {
+  return (
+    <div style={{ padding: 8 }}>
+      <UserBar />
+      <br />
+      <CreatePost user={user} />
+      <br />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
