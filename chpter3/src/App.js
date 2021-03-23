73ab1486f7685e6525cef5447c1215c63f710089
Achraf <achraf.rehioui.1@gmail.com>
2021-03-23 19:41:23 +0100

    passing the user to CreatePost

--- chpter3/src/App.js
+++ chpter3/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
 import UserBar from './user/UserBar';
 
-const user = 'Achraf'
+
 const posts = [
   { title: 'React Hooks', content: 'React!', author: 'Achraf' },
   { title: 'Using React Fragments', content: 'React Hooks!', author: 'Achraf' }
@@ -11,11 +11,14 @@ const posts = [
 
 
 export default function App() {
+
+  const [user, setUser] = useState('');
+
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
       <br />
-      <CreatePost user={user} />
+      {user && <CreatePost user={user} />}
       <br />
       <hr />
       <PostList posts={posts} />
--- chpter3/src/user/UserBar.js
+++ chpter3/src/user/UserBar.js
@@ -3,9 +3,8 @@ import Login from './Login';
 import Logout from './Logout';
 import Register from './Register';
 
-export default function UserBar() {
+export default function UserBar({ user, setUser }) {
 
-    const [user, setUser] = useState('');
     if (user) {
         return <Logout setUser={setUser} />
 
