660fbc61d51907a67b896ff8e4e422c126ccb778
Achraf <achraf.rehioui.1@gmail.com>
2021-03-24 16:30:45 +0100

    replace the posts state hook with a reducer hook

--- chpter3/src/App.js
+++ chpter3/src/App.js
@@ -9,17 +9,42 @@ const defaultPosts = [
   { title: 'Using React Fragments', content: 'React Hooks!', author: 'Achraf' }
 ]
 
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+    case 'REGISTER':
+      return action.username
+
+    case 'LOGOUT':
+      return ''
+
+    default:
+      throw new Error()
+  }
+}
+
+function postsReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_POST':
+      const newPost = { title: action.title, content: action.content, author: action.author }
+      return [newPost, ...state]
+
+    default:
+      throw new Error()
+  }
+}
 
 export default function App() {
 
   const [user, dispatchUser] = useReducer(userReducer, '');
-  const [posts, setPosts] = useState(defaultPosts);
+  const [posts, dispatchPosts] = useReducer(postsReducer, defaultPosts);
 
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={user} dispatch={dispatchUser} />
       <br />
-      {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
+      {user && <CreatePost user={user} posts={posts} dispatch=
+        {dispatchPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
